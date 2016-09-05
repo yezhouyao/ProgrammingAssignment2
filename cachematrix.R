f834b9dbe816d6a433ace4689d89fc618c441b58
yezhouyao <yezhoujoeyao@gmail.com>
2016-09-05 11:07:10 +0800

    correct mistake

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
         i <<- NULL
     }
     get <- function() x
-    setinverse <- function(solve) i <<- solve
+    setinverse <- function(inverse) i <<- inverse
     getinverse <- function() i
     list(set = set, get = get,
          setinverse = setinverse,
